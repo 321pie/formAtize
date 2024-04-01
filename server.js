bb57eedda13356840f76414cf3fa508625f96777
Andrew Rice <aprspie@gmail.com>
2024-04-01 10:10:14 -0700

    Fixed docker issues

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const path = require('path');
 const app = express();
 const port = 8000;
 
+app.use(express.static(__dirname + '/public' ));
+
 app.get('/', (req, res) => {
   // Read the contents of index.html
-  const htmlPath = path.join(__dirname, 'index.html');
+  const htmlPath = path.join(__dirname, 'public/index.html');
   const htmlContent = fs.readFileSync(htmlPath, 'utf8');
 
   // Send the HTML content as the response
